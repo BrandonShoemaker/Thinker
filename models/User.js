bc2a17fcfad358070042dfd6faeecc653cd78551
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-06-17 19:42:43 -0500

    Built models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const {Schema, model, Types} = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+UserSchema.path('email').validate(function(email){
+    const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email);
+});
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.reduce((total, friend) => total + friend.friends.length + 1, 0);
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
